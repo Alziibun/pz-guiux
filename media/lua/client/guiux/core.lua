0bcecbee184b9ced46c09d7e564f0fd83cae8d14
Alice S <snapp@null.net>
2022-06-13 18:08:57 -0400

    Attempt to iterate UI stack with `for` loop and hide said ui

--- media/lua/client/guiux/core.lua
+++ media/lua/client/guiux/core.lua
@@ -7,6 +7,8 @@ local function purgeVanillaUI()
     local _ui = UIManager.getUI();
     for index=0, _ui:size() -1 do
         print(index, _ui:get(index));
+        uiel = _ui:get(index)
+        uiel:setVisible(false)
     end
 end
 
