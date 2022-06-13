843471c2b211028f0bb7e8d574fb71091f143f66
Alice S <snapp@null.net>
2022-06-13 16:36:54 -0400

    Inventory debug

--- media/lua/client/guiux/inventory.lua
+++ media/lua/client/guiux/inventory.lua
@@ -1,6 +1,8 @@
 function openBag(_key)
     local player = getSpecificPlayer(0);
+    print("Played pressed".._key)
     if _key == 73 then -- open bag when the player presses I (73)
+
         player:Say("Open bag!");
     end
 
