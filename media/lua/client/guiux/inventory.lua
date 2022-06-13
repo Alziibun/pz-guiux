858ed6e072d8013ae73e0ee10eece231a785a5b7
Alice S <snapp@null.net>
2022-06-13 15:57:53 -0400

    test key pressing event

--- media/lua/client/guiux/inventory.lua
+++ media/lua/client/guiux/inventory.lua
@@ -1,6 +1,11 @@
 function openBag(_key)
     local player = getSpecificPlayer(0);
-    player:say("Open bag!")
+    if _key == 73 then
+        player:say("Open bag!");
+        else
+        player:say("A button was pressed");
+    end
+
 end
 
 Events.onKeyPressed.Add(openBag)
