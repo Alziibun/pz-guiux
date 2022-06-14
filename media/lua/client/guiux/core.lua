03a8f22a353178234be056772b73a9564fae4088
Alice S <snapp@null.net>
2022-06-13 20:00:39 -0400

    Attempt to hide vanilla inventory

--- media/lua/client/guiux/core.lua
+++ media/lua/client/guiux/core.lua
@@ -2,3 +2,4 @@
 newGUI = {
     gameVersion = getCore():getVersionNumber();
 };
+
--- media/lua/client/guiux/inventory.lua
+++ media/lua/client/guiux/inventory.lua
@@ -1,14 +1,8 @@
-invUI = UIManager():getInv()
-
 function openBag(_key)
     local player = getSpecificPlayer(0);
     if _key == 23 then -- open bag when the player presses I (73)
         player:Say("Opening bag!");
-        invUI:setVisible(false)
+        ISInventoryPane:hideButtons();
     end
 end
 Events.OnKeyPressed.Add(openBag)
-
-Events.OnCreateUI.Add(function()
-    InventoryContainer:getItemContainer()
-end)
